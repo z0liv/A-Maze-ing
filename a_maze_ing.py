bf0c9d3d7c5a50854cc4cef7a75cce31e15ca6e1
Oliver Marquez <omarquez@c2r1s4.42urduliz.com>
2026-09-09 13:12:36 +0200

    added basic exeption handlers, expecting model and function to validate config

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,16 +2,49 @@ import sys
 import os
 
 
+class InvalidConfigError(Exception):
+    def __init__(self, message: str = "[ERROR] Invalid configuration"):
+        self.message = message
+
+
+class ParamsError(Exception):
+    def __init__(
+        self,
+        message: str = "[ERROR] Program should have the correct parameters"
+    ) -> None:
+        self.message = message
+
+
 def main() -> None:
     if 'VIRTUAL_ENV' in os.environ:
-        args = sys.argv
-        if len(args) == 1 or len(args) > 2:
-            return (print("error"))
-        else:
-            content: str
-            with open(args[1]) as config:
-                content = config.read()
-                print(content)
+        try:
+            args = sys.argv
+            if len(args) == 1:
+                prefix = "[ERROR] Missing config file, "
+                raise ParamsError(
+                    prefix + f"expected 1 received {len(args) - 1}"
+                    )
+            elif len(args) > 2:
+                prefix = "[ERROR] Too many params, "
+                raise ParamsError(
+                    prefix + f"expected 1 received {len(args) - 1}"
+                    )
+            else:
+                content: str
+                with open(args[1]) as config:
+                    content = config.read()
+                    print(content)
+            """
+            Expects a function that detects invalid config
+            such as: impossible maze, bad syntax.
+            Must raise InvalidConfigError with a clear message
+            """
+        except InvalidConfigError as error:
+            print(error)
+        except ParamsError as error:
+            print(error)
+        except FileNotFoundError as error:
+            print("[ERROR]", error)
     else:
         print("WARNING: You're in the global environment!")
         print("To run this project you must be in a virtual environment")
