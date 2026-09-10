cd9f20ded8ba13bcd6362b201eeff91b092413a2
Julen Recio <julenreciotocado@gmail.com>
2026-09-10 14:13:18 +0200

    Modified custom exception and load config function to show various errors

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,12 +1,14 @@
 import sys
 import os
 from typing import Any
+from pydantic import ValidationError
 from src.config import Config
 
 
 class InvalidConfigError(Exception):
-    def __init__(self, message: str = "[ERROR] Invalid configuration"):
-        self.message = message
+    def __init__(self, messages: list[str]):
+        super().__init__("[ERROR] Invalid configuration: \n{}".format("\n".join(messages)))
+        self.messages = messages
 
 
 class ParamsError(Exception):
@@ -14,9 +16,10 @@ class ParamsError(Exception):
         self,
         message: str = "[ERROR] Program should have the correct parameters"
     ) -> None:
+        super().__init__(message)
         self.message = message
 
-def load_config(filename: str) -> Config:
+def load_config(filename: str) -> None:
     data: dict[str, Any] = {}
     with open(filename, "r") as config_file:
         for line in config_file:
@@ -25,8 +28,14 @@ def load_config(filename: str) -> Config:
             if (key.lower() == "entry" or key.lower() == "exit"):
                 value = (value.split(","))
             data[key.lower()] = value
-    configmodel: Config = Config(**data)
-    return configmodel
+    try:
+        configmodel: Config = Config(**data)
+        print(configmodel)
+    except ValidationError as val:
+        errors: list[str] = []
+        for error in val.errors():
+            errors.append(str(error.get("loc")[0]) + ": " + error.get("msg"))
+        raise InvalidConfigError(errors)
 
 def main() -> None:
     if 'VIRTUAL_ENV' in os.environ:
@@ -43,8 +52,7 @@ def main() -> None:
                     prefix + f"expected 1 received {len(args) - 1}"
                     )
             else:
-                model: Config = load_config(args[1])
-                print(model)
+                load_config(args[1])
             """
             Expects a function that detects invalid config
             such as: impossible maze, bad syntax.
